3f6756abb68a15e6ec80c6af6a84cc455b5af24a
Jakhongir Rasulov <jakhongirrasulov007@gmail.com>
2021-11-09 15:14:39 +0500

    adds cors proxy everywhere to make request from https to http

--- src/utils/Axios.js
+++ src/utils/Axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const PRODUCTION_BASE_URL =
-  "http://housingsystem-env.eba-cgcqvndq.eu-west-1.elasticbeanstalk.com/api/v1.0.0/";
+  "https://cors-everywhere.herokuapp.com/http://housingsystem-env.eba-cgcqvndq.eu-west-1.elasticbeanstalk.com/api/v1.0.0/";
 const DEVELOPMENT_BASE_URL = "http://localhost:5000/api/v1.0.0/";
 const instance = axios.create({ baseURL: PRODUCTION_BASE_URL });
 export default instance;
