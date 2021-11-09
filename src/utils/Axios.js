cf997879bbad3e7f4ecc9ed5bd2f6423866757a1
Jakhongir Rasulov <jakhongirrasulov007@gmail.com>
2021-11-09 14:57:15 +0500

    adds production url of the backend

--- src/utils/Axios.js
+++ src/utils/Axios.js
@@ -1,3 +1,6 @@
 import axios from "axios";
-const instance = axios.create({ baseURL: "http://localhost:5000/api/v1.0.0/" });
+const PRODUCTION_BASE_URL =
+  "http://housingsystem-env.eba-cgcqvndq.eu-west-1.elasticbeanstalk.com/api/v1.0.0/";
+const DEVELOPMENT_BASE_URL = "http://localhost:5000/api/v1.0.0/";
+const instance = axios.create({ baseURL: PRODUCTION_BASE_URL });
 export default instance;
