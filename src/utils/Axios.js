535d2255a78990f127912622a2a9be5c150910f2
Jakhongir Rasulov <jakhongirrasulov007@gmail.com>
2021-11-21 22:29:59 +0500

    changes url to workaround https problem

--- src/utils/Axios.js
+++ src/utils/Axios.js
@@ -2,6 +2,7 @@ import axios from "axios";
 const PRODUCTION_BASE_URL =
   "https://cors-everywhere.herokuapp.com/http://housingsystem-env.eba-cgcqvndq.eu-west-1.elasticbeanstalk.com/api/v1.0.0/";
 const DEVELOPMENT_BASE_URL = "http://localhost:5000/api/v1.0.0/";
-const NGROK_BASE_URL = "http://54f5-185-139-137-9.ngrok.io/";
+const NGROK_BASE_URL =
+  "https://cors-everywhere.herokuapp.com/http://54f5-185-139-137-9.ngrok.io/";
 const instance = axios.create({ baseURL: NGROK_BASE_URL });
 export default instance;
