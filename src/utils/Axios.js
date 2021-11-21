9b7ea54d06a3f7408358d7b7a4bda077500357c9
Jakhongir Rasulov <jakhongirrasulov007@gmail.com>
2021-11-21 22:36:38 +0500

    fixes url

--- src/utils/Axios.js
+++ src/utils/Axios.js
@@ -3,6 +3,6 @@ const PRODUCTION_BASE_URL =
   "https://cors-everywhere.herokuapp.com/http://housingsystem-env.eba-cgcqvndq.eu-west-1.elasticbeanstalk.com/api/v1.0.0/";
 const DEVELOPMENT_BASE_URL = "http://localhost:5000/api/v1.0.0/";
 const NGROK_BASE_URL =
-  "https://cors-everywhere.herokuapp.com/http://54f5-185-139-137-9.ngrok.io/";
+  "https://cors-everywhere.herokuapp.com/http://54f5-185-139-137-9.ngrok.io/api/v1.0.0/";
 const instance = axios.create({ baseURL: NGROK_BASE_URL });
 export default instance;
