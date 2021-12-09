708c31d9be6a9364953350a490141f0a200389b8
Jakhongir Rasulov <jakhongirrasulov007@gmail.com>
2021-12-09 22:21:47 +0500

    changes url

--- src/utils/Axios.js
+++ src/utils/Axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const PRODUCTION_BASE_URL =
   "https://cors-everywhere.herokuapp.com/http://housingsystembackend-env.eba-prmb6wib.eu-west-1.elasticbeanstalk.com/api/v1/";
-const DEVELOPMENT_BASE_URL = "http://localhost:5000/api/v1.0.0/";
-const NGROK_BASE_URL = "https://0e59-185-139-137-103.ngrok.io/api/v1.0.0/";
-const instance = axios.create({ baseURL: DEVELOPMENT_BASE_URL });
+const DEVELOPMENT_BASE_URL = "http://localhost:5000/api/v1/";
+const NGROK_BASE_URL = "https://0e59-185-139-137-103.ngrok.io/api/v1/";
+const instance = axios.create({ baseURL: PRODUCTION_BASE_URL });
 export default instance;
